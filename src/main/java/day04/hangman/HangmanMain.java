c10fb9434ac5a99b74a0519f302e36fab6f0bfc1
NagyLP <nagy.peter@pr.hu>
2021-11-18 16:34:56 +0100

    day06: Akasztófa (clean)

--- src/main/java/day04/hangman/HangmanMain.java
+++ src/main/java/day04/hangman/HangmanMain.java
@@ -16,7 +16,6 @@ public class HangmanMain {
         Scanner scanner = new Scanner(System.in);
 
         do {
-
             System.out.println("Itt tartasz: " + wordFound);
             System.out.println("Ennyi tipp: " + chances);
             System.out.println("Mi a következő tipped?");
@@ -31,11 +30,11 @@ public class HangmanMain {
 
         } while (!wordToFind.equals(wordFound) && (chances > 0));
 
-        if (wordFound.equals((wordToFind))) {
-            System.out.println("Ügyes!");
-        } else {
-            System.out.println("Vesztettél.");
-        }
+            if (wordFound.equals((wordToFind))) {
+                System.out.println("Ügyes!");
+            } else {
+                System.out.println("Vesztettél.");
+            }
     }
 
     private String updateWordFound(String wordToFind, String status, String guess) {
